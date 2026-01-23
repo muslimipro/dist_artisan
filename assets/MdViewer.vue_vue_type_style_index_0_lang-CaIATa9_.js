787452338d0477872472f67e62017fd2c4c0d107
Yertugan <muslim.ipro@gmail.com>
2026-01-23 10:35:23 +0500

    pers-info label fixed, content locale switching fixed, ide width fixed, course lesson click fixed, ide merged/redesigned, pibody all blocks added

---
+++ assets/MdViewer.vue_vue_type_style_index_0_lang-CaIATa9_.js
@@ -0,0 +1,1 @@
+import{d as n,o as r,l as p,L as s}from"./index-BVydjTcr.js";const m=n({__name:"MdViewer",props:{text:{}},setup(e){const t=e;return(c,_)=>{const o=s("v-md-preview");return r(),p(o,{text:t.text},null,8,["text"])}}});export{m as _};
