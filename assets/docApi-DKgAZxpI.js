7a43f0c38a5f4c44c938ca53c4e98e2665b92bba
atbrbk-fedora <atb.rbk@gmail.com>
2026-01-27 15:14:19 +0500

    blocks: editor, project view

---
+++ assets/docApi-DKgAZxpI.js
@@ -0,0 +1,1 @@
+import{S as t}from"./index-DCICvVE8.js";const s={getDocs:o};async function o(){return await t.get("v2/docs")}export{s as D};
--- assets/index-DOruQYWF.js
+++
@@ -1,1 +0,0 @@
-import{bb as r}from"./index-BhgOSbZC.js";var e=r();export{e as O};
